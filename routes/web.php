dc08d5be96aae95b7357067b1d21992772a14c9b
salgadosb <sebastesb1986@gmail.com>
2024-07-21 16:06:23 -0500

    Add desing welcome app

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Captura todas las rutas
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*');
