10a3dd03882314585d385dd5decc158e2b1c128a
salgadosb <sebastesb1986@gmail.com>
2024-07-22 00:35:33 -0500

    Admin interface

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,7 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+axios.defaults.baseURL = 'https://salgado-school-30c338bdf4e3.herokuapp.com';
 
 store.dispatch('auth/attempt', localStorage.getItem('token'))
 .then( ()=> {
