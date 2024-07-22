5b823487bfe4ea591e6dfdeb674ed74108ba5b20
salgadosb <sebastesb1986@gmail.com>
2024-07-22 03:49:32 -0500

    Admin interface2

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         }),
     ],
     build: {
-        outDir: 'public/build/.vite', // Asegúrate de que esta ruta coincida con la configuración de Laravel
+        outDir: 'public/build/', // Asegúrate de que esta ruta coincida con la configuración de Laravel
         manifest: true,
         // Otros ajustes de construcción según tus necesidades
     },
