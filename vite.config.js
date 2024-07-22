d3bdc092103bf3ba3ba40e6a360beea1923ff9e2
salgadosb <sebastesb1986@gmail.com>
2024-07-22 02:53:20 -0500

    Admin interface2

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
         }),
     ],
     build: {
-
-        manifest: true,
+        minify: true,
+        brotliSize: true,
+        sourcemap: false,
     },
 });
