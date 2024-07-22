16fbff7445e3a44567ac1265b0903a9f5175537c
salgadosb <sebastesb1986@gmail.com>
2024-07-22 02:42:57 -0500

    Admin interface2

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: 'https://salgado-school-30c338bdf4e3.herokuapp.com/', // Reemplaza con el nombre de tu aplicación en Heroku
     plugins: [
         vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             mode: 'production',
-            extract: true, // Extraer CSS en un archivo separado
+            extract: true,
             minify: true,
         }),
     ],
