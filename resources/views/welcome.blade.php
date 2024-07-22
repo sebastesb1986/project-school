4a7e7a65a8628806a7de98dc244e0e76499bcad3
salgadosb <sebastesb1986@gmail.com>
2024-07-22 04:40:19 -0500

    Admin interface3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Aplicación</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite('resources/css/app.css')
     </head>
     <body>
         <div id="app"></div>
+        @vite('resources/js/app.js')
     </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,11 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
         }),
     ],
+    server: {
+        https: true, // Habilita HTTPS para el servidor de desarrollo
+    },
     build: {
+        assetsDir: 'build/assets',
         outDir: 'public/build/', // Asegúrate de que esta ruta coincida con la configuración de Laravel
         manifest: 'manifest.json',
         // Otros ajustes de construcción según tus necesidades
